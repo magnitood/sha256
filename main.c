af8da3e199f243332e12d5c6a59443aec596e127
Parthasarathi <theeparthasarathi@gmail.com>
2025-05-07 20:16:26 +0530

    core: change debug prints
    Made debug prints use the standard library error messages instead of
    custom ones

--- main.c
+++ main.c
@@ -3,25 +3,14 @@
 #include <errno.h>
 #include "sha256.h"
 
-void handle_error()
-{
-	switch(errno) {
-		case EACCES:
-			perror("Cannot open file\n");
-			break;
-		default:
-			perror("Error\n");
-			break;
-	}
-}
-
 int main(int argc, char *argv[])
 {
 	FILE *fp;
 	if (argc >= 2) {
 		fp = fopen(argv[1], "r");
 		if (fp == NULL) {
-			handle_error();
+			fprintf(stderr, "fopen: ");
+			perror(NULL);
 			exit(EXIT_FAILURE);
 		}
 	} else {
